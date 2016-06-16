a63f20a4126a104bea3220c6c77c81500127729a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-06-16 15:23:27 -0700

    update user generation

--- populate_db.py
+++ populate_db.py
@@ -8,13 +8,13 @@ def main():
     collection = MongoClient()["rest_bank"]["users"]
 
     # Ask for data to store
-    user = raw_input("Enter your username: ")
+    name = raw_input("Enter your username: ")
     password = raw_input("Enter your password: ")
     pass_hash = generate_password_hash(password, method='pbkdf2:sha256')
 
     # Insert the user in the DB
     try:
-        collection.insert({"_id": user, "password": pass_hash})
+        collection.insert({"name": name, "password": pass_hash, "email": "", "role": 2})
         print "User created."
     except DuplicateKeyError:
         print "User already present in DB."
